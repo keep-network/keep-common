d20c65c7ffb430c0d98d43a7e5d96b2f2f388f5b
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-09-26 11:17:06 +0200

    Utilities for bytes
    Package byteutils provides helper utilities for working with bytes

---
+++ pkg/utils/byteutils/byteutils.go
@@ -0,0 +1,39 @@
+// Package byteutils provides helper utilities for working with bytes.
+package byteutils
+
+import (
+	"fmt"
+)
+
+// LeftPadTo32Bytes appends zeros to bytes slice to make it exactly 32 bytes long.
+func LeftPadTo32Bytes(bytes []byte) ([]byte, error) {
+	expectedByteLen := 32
+	if len(bytes) > expectedByteLen {
+		return nil, fmt.Errorf(
+			"cannot pad %v byte array to %v bytes", len(bytes), expectedByteLen,
+		)
+	}
+
+	result := make([]byte, 0)
+	if len(bytes) < expectedByteLen {
+		result = make([]byte, expectedByteLen-len(bytes))
+	}
+	result = append(result, bytes...)
+
+	return result, nil
+}
+
+// BytesTo32Byte converts bytes slice to a 32-byte array. It left pads the array
+// with zeros in case of a slice shorter than 32-byte.
+func BytesTo32Byte(bytes []byte) ([32]byte, error) {
+	var result [32]byte
+
+	paddedBytes, err := LeftPadTo32Bytes(bytes)
+	if err != nil {
+		return result, err
+	}
+
+	copy(result[:], paddedBytes[:32])
+
+	return result, nil
+}

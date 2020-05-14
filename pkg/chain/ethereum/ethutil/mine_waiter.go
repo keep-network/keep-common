164827f3dbbb90b8fd5879e23e407b66ee317528
Piotr Dyraga <piotr.dyraga@keep.network>
2020-05-14 18:50:27 +0200

    MiningWaiter implementation
    MiningWaiter allows to block the execution until the given transaction
    is mined. We will use it to increase gas price after a certain timeout.

---
+++ pkg/chain/ethereum/ethutil/mine_waiter.go
@@ -0,0 +1,33 @@
+package ethutil
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// MiningWaiter allows to block the execution until the given transaction is
+// mined.
+type MiningWaiter struct {
+	backend bind.DeployBackend
+}
+
+// NewMiningWaiter creates a new MiningWaiter instance for the provided
+// client backend.
+func NewMiningWaiter(backend bind.DeployBackend) *MiningWaiter {
+	return &MiningWaiter{backend}
+}
+
+// WaitMined blocks the current execution until the transaction with the given
+// hash is mined. Execution is blocked until the transaction is mined or until
+// the given timeout passes.
+func (mw *MiningWaiter) WaitMined(
+	timeout time.Duration, 
+	tx *types.Transaction,
+) (*types.Receipt, error) {
+	ctx, cancel := context.WithTimeout(timeout)
+	defer cancel()
+
+	return bind.WaitMined(ctx, mw.backend, tx)
+}

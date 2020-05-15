ac571524a54beb9b95f00c94e949862ab718603d
Piotr Dyraga <piotr.dyraga@keep.network>
2020-05-15 20:06:55 +0200

    Added docs to configuration options

--- pkg/chain/ethereum/config.go
+++ pkg/chain/ethereum/config.go
@@ -39,8 +39,16 @@ type Config struct {
 
 	Account Account
 
+	// MiningCheckInterval is the interval in which transaction
+	// mining status is checked. If the transaction is not mined within this
+	// time, the gas price is increased and transaction is resubmitted.
 	MiningCheckInterval int
 
+	// MaxGasPrice specifies the maximum gas price the client is
+	// willing to pay for the transaction to be mined. The offered transaction
+	// gas price can not be higher than the max gas price value. If the maximum
+	// allowed gas price is reached, no further resubmission attempts are
+	// performed.
 	MaxGasPrice uint64
 }
 

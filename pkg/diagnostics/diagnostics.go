5f75ddd05b21d02b78ad839636a2910e57ed6b9c
Alexander Ivash <elderorb@gmail.com>
2020-08-19 21:37:41 +0300

    address some comments from PR

--- pkg/diagnostics/diagnostics.go
+++ pkg/diagnostics/diagnostics.go
@@ -48,6 +48,8 @@ func (r *DiagnosticsRegistry) EnableServer(port int) {
 	}()
 }
 
+// Registers diagnostics source callback with a given name. Name will be used as a key and callback result as a value in JSON object
+// during composing diagnostics JSON. Note: function will override existing diagnostics source on attempt to register another one with the same name
 func (r *DiagnosticsRegistry) RegisterSource(name string, source func() string) {
 	r.diagnosticsMutex.Lock()
 	defer r.diagnosticsMutex.Unlock()
@@ -72,6 +74,7 @@ func (r *DiagnosticsRegistry) exposeDiagnostics() string {
 
 	bytes, err := json.Marshal(diagnostics)
 	if err != nil {
+		logger.Errorf("diagnostics JSON serialization error: [%v]", err)
 		return ""
 	}
 

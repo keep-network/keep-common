ced5a6dc69e5849f97e186dfd268fe9b727cf90e
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-08-19 14:58:12 +0200

    Rework rate limiter tests

--- pkg/chain/ethereum/ethutil/rate_limiter_test.go
+++ pkg/chain/ethereum/ethutil/rate_limiter_test.go
@@ -3,6 +3,7 @@ package ethutil
 import (
 	"context"
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"math/big"
@@ -34,95 +35,7 @@ func TestRateLimiter(t *testing.T) {
 		},
 	)
 
-	tests := map[string]struct {
-		function func() error
-	}{
-		"test CodeAt rate limiting": {
-			function: func() error {
-				_, err := rateLimitingBackend.CodeAt(
-					context.Background(),
-					[20]byte{},
-					nil,
-				)
-				return err
-			},
-		},
-		"test CallContract rate limiting": {
-			function: func() error {
-				_, err := rateLimitingBackend.CallContract(
-					context.Background(),
-					ethereum.CallMsg{},
-					nil,
-				)
-				return err
-			},
-		},
-		"test PendingCodeAt rate limiting": {
-			function: func() error {
-				_, err := rateLimitingBackend.PendingCodeAt(
-					context.Background(),
-					[20]byte{},
-				)
-				return err
-			},
-		},
-		"test PendingNonceAt rate limiting": {
-			function: func() error {
-				_, err := rateLimitingBackend.PendingNonceAt(
-					context.Background(),
-					[20]byte{},
-				)
-				return err
-			},
-		},
-		"test SuggestGasPrice rate limiting": {
-			function: func() error {
-				_, err := rateLimitingBackend.SuggestGasPrice(
-					context.Background(),
-				)
-				return err
-			},
-		},
-		"test EstimateGas rate limiting": {
-			function: func() error {
-				_, err := rateLimitingBackend.EstimateGas(
-					context.Background(),
-					ethereum.CallMsg{},
-				)
-				return err
-			},
-		},
-		"test SendTransaction rate limiting": {
-			function: func() error {
-				err := rateLimitingBackend.SendTransaction(
-					context.Background(),
-					nil,
-				)
-				return err
-			},
-		},
-		"test FilterLogs rate limiting": {
-			function: func() error {
-				_, err := rateLimitingBackend.FilterLogs(
-					context.Background(),
-					ethereum.FilterQuery{},
-				)
-				return err
-			},
-		},
-		"test SubscribeFilterLogs rate limiting": {
-			function: func() error {
-				_, err := rateLimitingBackend.SubscribeFilterLogs(
-					context.Background(),
-					ethereum.FilterQuery{},
-					nil,
-				)
-				return err
-			},
-		},
-	}
-
-	for testName, test := range tests {
+	for testName, test := range getTests(rateLimitingBackend) {
 		t.Run(testName, func(t *testing.T) {
 			wg := sync.WaitGroup{}
 			wg.Add(requests)
@@ -189,6 +102,148 @@ func TestRateLimiter(t *testing.T) {
 	}
 }
 
+func TestRateLimiter_RequestsPerSecondLimitOnly(t *testing.T) {
+	requestsPerSecondLimit := 500
+	concurrencyLimit := 0 // disable the concurrency limit
+	acquirePermitTimeout := time.Minute
+	requests := 500
+	requestDuration := 10 * time.Millisecond
+
+	backend := &mockBackend{
+		requestDuration,
+		make([]string, 0),
+		sync.Mutex{},
+	}
+
+	rateLimitingBackend := WrapRateLimiting(
+		backend,
+		&RateLimiterConfig{
+			RequestsPerSecondLimit: requestsPerSecondLimit,
+			ConcurrencyLimit:       concurrencyLimit,
+			AcquirePermitTimeout:   acquirePermitTimeout,
+		},
+	)
+
+	for testName, test := range getTests(rateLimitingBackend) {
+		t.Run(testName, func(t *testing.T) {
+			wg := sync.WaitGroup{}
+			wg.Add(requests)
+
+			startSignal := make(chan struct{})
+
+			for i := 0; i < requests; i++ {
+				go func() {
+					<-startSignal
+
+					err := test.function()
+					if err != nil {
+						t.Errorf("unexpected error: [%v]", err)
+					}
+
+					wg.Done()
+				}()
+			}
+
+			startTime := time.Now()
+			close(startSignal)
+
+			wg.Wait()
+
+			duration := time.Now().Sub(startTime)
+			averageRequestsPerSecond := float64(requests) / duration.Seconds()
+
+			// The actual average can exceed the limit a little bit.
+			// Here we set the maximum acceptable deviation to 2%.
+			maxDeviation := 0.02
+
+			if averageRequestsPerSecond > (1+maxDeviation)*float64(requestsPerSecondLimit) {
+				t.Errorf(
+					"average requests per second exceeded the limit\n"+
+						"limit:  [%v]\n"+
+						"actual: [%v]",
+					requestsPerSecondLimit,
+					averageRequestsPerSecond,
+				)
+			}
+		})
+	}
+}
+
+func TestRateLimiter_ConcurrencyLimitOnly(t *testing.T) {
+	requestsPerSecondLimit := 0 // disable the requests per second limit
+	concurrencyLimit := 50
+	acquirePermitTimeout := time.Minute
+	requests := 500
+	requestDuration := 10 * time.Millisecond
+
+	backend := &mockBackend{
+		requestDuration,
+		make([]string, 0),
+		sync.Mutex{},
+	}
+
+	rateLimitingBackend := WrapRateLimiting(
+		backend,
+		&RateLimiterConfig{
+			RequestsPerSecondLimit: requestsPerSecondLimit,
+			ConcurrencyLimit:       concurrencyLimit,
+			AcquirePermitTimeout:   acquirePermitTimeout,
+		},
+	)
+
+	for testName, test := range getTests(rateLimitingBackend) {
+		t.Run(testName, func(t *testing.T) {
+			wg := sync.WaitGroup{}
+			wg.Add(requests)
+
+			startSignal := make(chan struct{})
+
+			for i := 0; i < requests; i++ {
+				go func() {
+					<-startSignal
+
+					err := test.function()
+					if err != nil {
+						t.Errorf("unexpected error: [%v]", err)
+					}
+
+					wg.Done()
+				}()
+			}
+
+			close(startSignal)
+
+			wg.Wait()
+
+			maxConcurrency := 0
+			temporaryConcurrency := 0
+			for _, event := range backend.events {
+				if event == "start" {
+					temporaryConcurrency++
+				}
+
+				if event == "end" {
+					temporaryConcurrency--
+				}
+
+				if temporaryConcurrency > maxConcurrency {
+					maxConcurrency = temporaryConcurrency
+				}
+			}
+
+			if maxConcurrency > concurrencyLimit {
+				t.Errorf(
+					"max concurrency exceeded the limit\n"+
+						"limit:  [%v]\n"+
+						"actual: [%v]",
+					concurrencyLimit,
+					maxConcurrency,
+				)
+			}
+		})
+	}
+}
+
 func TestRateLimiter_AcquirePermitTimout(t *testing.T) {
 	requestsPerSecondLimit := 1
 	concurrencyLimit := 1
@@ -215,14 +270,7 @@ func TestRateLimiter_AcquirePermitTimout(t *testing.T) {
 	wg.Add(requests)
 
 	startSignal := make(chan struct{})
-	errorChannel := make(chan error, requests)
-	errors := make([]error, 0)
-
-	go func() {
-		for e := range errorChannel {
-			errors = append(errors, e)
-		}
-	}()
+	errors := make(chan error, requests)
 
 	for i := 0; i < requests; i++ {
 		go func() {
@@ -230,7 +278,7 @@ func TestRateLimiter_AcquirePermitTimout(t *testing.T) {
 
 			err := rateLimitingBackend.SendTransaction(context.Background(), nil)
 			if err != nil {
-				errorChannel <- err
+				errors <- err
 			}
 
 			wg.Done()
@@ -241,11 +289,12 @@ func TestRateLimiter_AcquirePermitTimout(t *testing.T) {
 
 	wg.Wait()
 
+	close(errors)
 	if len(errors) == 0 {
 		t.Fatalf("at least one timeout error should be present")
 	}
 
-	for _, e := range errors {
+	for e := range errors {
 		if !strings.Contains(e.Error(), "context deadline") {
 			t.Errorf(
 				"error should be related with the context deadline\n"+
@@ -347,4 +396,94 @@ func (mb *mockBackend) SubscribeFilterLogs(
 ) (ethereum.Subscription, error) {
 	mb.mockRequest()
 	return nil, nil
+}
+
+func getTests(
+	backend bind.ContractBackend,
+) map[string]struct{ function func() error } {
+	return map[string]struct{ function func() error }{
+		"test CodeAt": {
+			function: func() error {
+				_, err := backend.CodeAt(
+					context.Background(),
+					[20]byte{},
+					nil,
+				)
+				return err
+			},
+		},
+		"test CallContract": {
+			function: func() error {
+				_, err := backend.CallContract(
+					context.Background(),
+					ethereum.CallMsg{},
+					nil,
+				)
+				return err
+			},
+		},
+		"test PendingCodeAt": {
+			function: func() error {
+				_, err := backend.PendingCodeAt(
+					context.Background(),
+					[20]byte{},
+				)
+				return err
+			},
+		},
+		"test PendingNonceAt": {
+			function: func() error {
+				_, err := backend.PendingNonceAt(
+					context.Background(),
+					[20]byte{},
+				)
+				return err
+			},
+		},
+		"test SuggestGasPrice": {
+			function: func() error {
+				_, err := backend.SuggestGasPrice(
+					context.Background(),
+				)
+				return err
+			},
+		},
+		"test EstimateGas": {
+			function: func() error {
+				_, err := backend.EstimateGas(
+					context.Background(),
+					ethereum.CallMsg{},
+				)
+				return err
+			},
+		},
+		"test SendTransaction": {
+			function: func() error {
+				err := backend.SendTransaction(
+					context.Background(),
+					nil,
+				)
+				return err
+			},
+		},
+		"test FilterLogs": {
+			function: func() error {
+				_, err := backend.FilterLogs(
+					context.Background(),
+					ethereum.FilterQuery{},
+				)
+				return err
+			},
+		},
+		"test SubscribeFilterLogs": {
+			function: func() error {
+				_, err := backend.SubscribeFilterLogs(
+					context.Background(),
+					ethereum.FilterQuery{},
+					nil,
+				)
+				return err
+			},
+		},
+	}
 }

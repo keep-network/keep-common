93769e72e76fb9308d21001755b406153faa7476
Piotr Dyraga <piotr.dyraga@keep.network>
2021-01-05 22:48:16 +0100

    Accept subscriptionFailed and thresholdViolated in WithResubscription
    The mechanism monitors the time elapsed between resubscription attempts and
    if it is shorter than the specificed alertThreshold, it writes the time
    elapsed between resubscription attempts to the thresholdViolated channel to
    alarm about potential problems with the stability of the subscription.
    
    Every error returned by the wrapped subscription function, is written to
    subscriptionFailed channel.
    
    If the calling code is interested in reading from thresholdViolated and/or
    subscriptionFailed channel, appropriate readers need to be set up *before*
    WithResubscription is called.
    
    Writes to thresholdViolated and subscriptionFailed channels are non-blocking
    and are not stopping resubscription attempts.
    
    Those two channels will let the client code log warning/errors or even
    kill the subscription in case alrt threshold is violated too many times.

--- pkg/chain/ethereum/ethutil/resubscribe.go
+++ pkg/chain/ethereum/ethutil/resubscribe.go
@@ -6,33 +6,57 @@ import (
 	"github.com/ethereum/go-ethereum/event"
 )
 
-// WithResubscription wraps the resubscription function to call it repeatedly
-// to keep a subscription established. When subscription is established, it is
-// monitored and in the case of a failure, resubscription is attempted by
-// calling the function again.
+// WithResubscription wraps the subscribe function to call it repeatedly
+// to keep a subscription alive. When a subscription is established, it is
+// monitored and in the case of a failure, resubscribe is attempted by
+// calling the subscribe function again.
 //
-// The mechanism applies backoff between calls to resubscribe function.
+// The mechanism applies backoff between resubscription attempts.
 // The time between calls is adapted based on the error rate, but will never
 // exceed backoffMax.
 //
 // The mechanism monitors the time elapsed between resubscription attempts and
-// if it is shorter than the specificed alertThreshold, it calls the alertFn
-// to alarm about potential problems with the stability of the subscription.
+// if it is shorter than the specificed alertThreshold, it writes the time
+// elapsed between resubscription attempts to the thresholdViolated channel to
+// alarm about potential problems with the stability of the subscription.
+//
+// Every error returned by the wrapped subscription function, is written to
+// subscriptionFailed channel.
+//
+// If the calling code is interested in reading from thresholdViolated and/or
+// subscriptionFailed channel, appropriate readers need to be set up *before*
+// WithResubscription is called.
+//
+// Writes to thresholdViolated and subscriptionFailed channels are non-blocking
+// and are not stopping resubscription attempts.
 func WithResubscription(
 	backoffMax time.Duration,
-	resubscribeFn event.ResubscribeFunc,
+	resubscribeFn event.ResubscribeFunc,	
 	alertThreshold time.Duration,
-	alertFn func(),
+	thresholdViolated chan<- time.Duration,
+	subscriptionFailed chan<- error,
 ) event.Subscription {
 	lastAttempt := time.Time{}
 	wrappedResubscribeFn := func(ctx context.Context) (event.Subscription, error) {
 		now := time.Now()
 		elapsed := now.Sub(lastAttempt)
 		if elapsed < alertThreshold {
-			alertFn()
+			select {
+			case thresholdViolated <- elapsed: 
+			default:
+			}
 		}
+		
 		lastAttempt = now
-		return resubscribeFn(ctx)
+
+		sub, err := resubscribeFn(ctx)
+		if err != nil {
+			select {
+			case subscriptionFailed <- err:
+			default:
+			}
+		}
+		return sub, err
 	}
 
 	return event.Resubscribe(backoffMax, wrappedResubscribeFn)
--- pkg/chain/ethereum/ethutil/resubscribe_test.go
+++ pkg/chain/ethereum/ethutil/resubscribe_test.go
@@ -9,21 +9,66 @@ import (
 	"github.com/ethereum/go-ethereum/event"
 )
 
+func TestEmitOriginalError(t *testing.T) {
+	backoffMax := 100 * time.Millisecond
+	alertThreshold := 100 * time.Millisecond
+
+	failedOnce := false
+	resubscribeFn := func(ctx context.Context) (event.Subscription, error) {
+		if !failedOnce {
+			failedOnce = true
+			return nil, fmt.Errorf("wherever I go, he goes")
+		}
+		delegate := event.NewSubscription(func(unsubscribed <-chan struct{}) error {
+			return nil
+		})
+		return delegate, nil
+	}
+
+	thresholdViolated := make(chan time.Duration, 10)
+	subscriptionFailed := make(chan error, 10)
+	subscription := WithResubscription(
+		backoffMax,
+		resubscribeFn,
+		alertThreshold,
+		thresholdViolated,
+		subscriptionFailed,
+	)
+	<-subscription.Err()
+
+	// Subscription failed one time so there should be one error in the channel.
+	subscriptionFailCount := len(subscriptionFailed)
+	if subscriptionFailCount != 1 {
+		t.Fatalf(
+			"subscription failure reported [%v] times, expected [1]",
+			subscriptionFailCount,
+		)
+	}
+
+	// That failure should refer the original error.
+	expectedFailMessage := "wherever I go, he goes"
+	err := <-subscriptionFailed
+	if err.Error() != expectedFailMessage {
+		t.Fatalf(
+			"unexpected subscription error message\nexpected: [%v]\nactual:   [%v]",
+			expectedFailMessage,
+			err.Error(),
+		)
+	}
+}
+
 func TestResubscribeAboveThreshold(t *testing.T) {
 	backoffMax := 100 * time.Millisecond
 	alertThreshold := 100 * time.Millisecond
 
-	alertFnCalls := 0
-	alertFn := func() {
-		alertFnCalls++
-	}
+	plannedSubscriptionFailures := 3
+	elapsedBetweenFailures := 150 * time.Millisecond
 
-	subscriptionFailures := 3
 	resubscribeFnCalls := 0
 	resubscribeFn := func(ctx context.Context) (event.Subscription, error) {
 		resubscribeFnCalls++
-		time.Sleep(150 * time.Millisecond) // 150 > 100, above alert threshold
-		if resubscribeFnCalls <= subscriptionFailures {
+		time.Sleep(elapsedBetweenFailures) // 150ms > 100ms, above alert threshold
+		if resubscribeFnCalls <= plannedSubscriptionFailures {
 			return nil, fmt.Errorf("this is the way")
 		}
 		delegate := event.NewSubscription(func(unsubscribed <-chan struct{}) error {
@@ -32,31 +77,48 @@ func TestResubscribeAboveThreshold(t *testing.T) {
 		return delegate, nil
 	}
 
+	thresholdViolated := make(chan time.Duration, 10)
+	subscriptionFailed := make(chan error, 10)
 	subscription := WithResubscription(
 		backoffMax,
 		resubscribeFn,
 		alertThreshold,
-		alertFn,
+		thresholdViolated,
+		subscriptionFailed,
 	)
 	<-subscription.Err()
 
-	// No calls to alertFn expected. Alert threshold is set to 100ms and no
-	// there were no resubscription attempts in a time shorter than 150ms one
-	// after another.
-	if alertFnCalls != 0 {
-		t.Fatalf("alert triggered [%v] times, expected none", alertFnCalls)
+	// Nothing expected in thresholdViolated channel.
+	// Alert threshold is set to 100ms and there were no resubscription attempts
+	// in a time shorter than 150ms one after another.
+	violationCount := len(thresholdViolated)
+	if violationCount != 0 {
+		t.Fatalf(
+			"threshold violation reported [%v] times, expected none",
+			violationCount,
+		)
 	}
 
-	// Subscription failed `subscriptionFailures` times and resubscription
-	// function should be called `subscriptionFailures + 1` times - one time
+	// Subscription failed plannedSubscriptionFailures times and resubscription
+	// function should be called plannedSubscriptionFailures + 1 times. One time
 	// for each failure and one time at the end - that subscription was
 	// successful and had not to be retried.
-	expectedResubscriptionCalls := subscriptionFailures + 1
+	expectedResubscriptionCalls := plannedSubscriptionFailures + 1
 	if resubscribeFnCalls != expectedResubscriptionCalls {
 		t.Fatalf(
 			"resubscription called [%v] times, expected [%v]",
 			resubscribeFnCalls,
 			expectedResubscriptionCalls,
+		)
+	}
+
+	// Expect all subscription failures to be reported.
+	subscriptionFailCount := len(subscriptionFailed)
+	if subscriptionFailCount != plannedSubscriptionFailures {
+		t.Fatalf(
+			"subscription failure reported [%v] times, expected [%v]",
+			subscriptionFailCount,
+			plannedSubscriptionFailures,
 		)
 	}
 }
@@ -65,17 +127,14 @@ func TestResubscribeBelowThreshold(t *testing.T) {
 	backoffMax := 50 * time.Millisecond
 	alertThreshold := 100 * time.Millisecond
 
-	alertFnCalls := 0
-	alertFn := func() {
-		alertFnCalls++
-	}
+	plannedSubscriptionFailures := 5
+	elapsedBetweenFailures := 50 * time.Millisecond
 
-	subscriptionFailures := 5
 	resubscribeFnCalls := 0
 	resubscribeFn := func(ctx context.Context) (event.Subscription, error) {
 		resubscribeFnCalls++
-		time.Sleep(50 * time.Millisecond) // 50 < 100, below alert threshold
-		if resubscribeFnCalls <= subscriptionFailures {
+		time.Sleep(elapsedBetweenFailures) // 50ms < 100ms, below alert threshold
+		if resubscribeFnCalls <= plannedSubscriptionFailures {
 			return nil, fmt.Errorf("i have spoken")
 		}
 		delegate := event.NewSubscription(func(unsubscribed <-chan struct{}) error {
@@ -84,33 +143,118 @@ func TestResubscribeBelowThreshold(t *testing.T) {
 		return delegate, nil
 	}
 
+	thresholdViolated := make(chan time.Duration, 10)
+	subscriptionFailed := make(chan error, 10)
 	subscription := WithResubscription(
 		backoffMax,
 		resubscribeFn,
 		alertThreshold,
-		alertFn,
+		thresholdViolated,
+		subscriptionFailed,
 	)
 	<-subscription.Err()
 
-	fmt.Printf("resubscribe count = [%v]\n", resubscribeFnCalls)
-	// Alert function should be called for each subscription failure if the time
-	// between the previous resubscription was shorter than the threshold.
+	// Threshold violaton should be reported for each subscription failure if
+	// the time elapsed since the previous resubscription was shorter than the
+	// threshold.
 	// In this test, alert threshold is set to 100ms and delays between failures
-	// are just 50ms.
-	expectedAlertFnCalls := subscriptionFailures
-	if alertFnCalls != expectedAlertFnCalls {
+	// are just 50ms. Thus, we expect the same number of threshold violations as
+	// resubscription attempts.
+	violationCount := len(thresholdViolated)
+	if violationCount != plannedSubscriptionFailures {
 		t.Fatalf(
-			"alert triggered [%v] times, expected [%v]",
-			alertFnCalls,
-			expectedAlertFnCalls,
+			"threshold violation reported [%v] times, expected [%v]",
+			violationCount,
+			plannedSubscriptionFailures,
 		)
 	}
 
-	// Subscription failed `subscriptionFailures` times and resubscription
-	// function should be called `subscriptionFailures + 1` times - one time
+	// All violations reported should have correct values - all of them should
+	// be longer than the time elapsed between failures and shorter than the
+	// alert threshold. It is not possible to assert on a precise value.
+	for i := 0; i < violationCount; i++ {
+		violation := <-thresholdViolated
+		if violation < elapsedBetweenFailures {
+			t.Fatalf(
+				"violation reported should be longer than the time elapsed "+
+					"between failures; is: [%v] and should be longer than [%v]",
+				violation,
+				elapsedBetweenFailures,
+			)
+		}
+		if violation > alertThreshold {
+			t.Fatalf(
+				"violation reported should be shorter than the alert threshold; "+
+					"; is: [%v] and should be shorter than [%v]",
+				violation,
+				alertThreshold,
+			)
+		}
+	}
+
+	// Subscription failed plannedSubscriptionFailures times and resubscription
+	// function should be called plannedSubscriptionFailures + 1 times. One time
 	// for each failure and one time at the end - that subscription was
 	// successful and had not to be retried.
-	expectedResubscriptionCalls := subscriptionFailures + 1
+	expectedResubscriptionCalls := plannedSubscriptionFailures + 1
+	if resubscribeFnCalls != expectedResubscriptionCalls {
+		t.Fatalf(
+			"resubscription called [%v] times, expected [%v]",
+			resubscribeFnCalls,
+			expectedResubscriptionCalls,
+		)
+	}
+
+	// Expect all subscription failures to be reported.
+	subscriptionFailCount := len(subscriptionFailed)
+	if subscriptionFailCount != plannedSubscriptionFailures {
+		t.Fatalf(
+			"subscription failure reported [%v] times, expected [%v]",
+			subscriptionFailCount,
+			plannedSubscriptionFailures,
+		)
+	}
+}
+
+func TestDoNotBlockOnChannelWrites(t *testing.T) {
+	backoffMax := 50 * time.Millisecond
+	alertThreshold := 100 * time.Millisecond
+
+	plannedSubscriptionFailures := 5
+	elapsedBetweenFailures := 10 * time.Millisecond
+
+	resubscribeFnCalls := 0
+	resubscribeFn := func(ctx context.Context) (event.Subscription, error) {
+		resubscribeFnCalls++
+		time.Sleep(elapsedBetweenFailures) // 10ms < 100ms, below alert threshold
+		if resubscribeFnCalls <= plannedSubscriptionFailures {
+			return nil, fmt.Errorf("Groku?")
+		}
+		delegate := event.NewSubscription(func(unsubscribed <-chan struct{}) error {
+			return nil
+		})
+		return delegate, nil
+	}
+
+	// Non-buffered channels with no receivers
+	thresholdViolated := make(chan time.Duration)
+	subscriptionFailed := make(chan error)
+
+	subscription := WithResubscription(
+		backoffMax,
+		resubscribeFn,
+		alertThreshold,
+		thresholdViolated,
+		subscriptionFailed,
+	)
+	<-subscription.Err()
+
+	// Subscription failed plannedSubscriptionFailures times and resubscription
+	// function should be called plannedSubscriptionFailures + 1 times. One time
+	// for each failure and one time at the end - that subscription was
+	// successful and had not to be retried. No resubscription attempt should be
+	// blocked by the lack of channel receivers on non-buffered channels.
+	expectedResubscriptionCalls := plannedSubscriptionFailures + 1
 	if resubscribeFnCalls != expectedResubscriptionCalls {
 		t.Fatalf(
 			"resubscription called [%v] times, expected [%v]",

f37e90dcdb1a87988fe4a62b4702ec87c815e746
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-02-12 12:23:03 +0100

    Duplicate some `ethutil` functions
    Two functions `CallAtBlock` and `EstimateGas`
    should be duplicated for Celo as well. They
    are tightly coupled with the specific
    `go-ethereum` package so there is no reasonable
    way to make them friendly for all ETH-like chains.
    Hiding `go-ethereum` specific types behind
    interfaces doesn't seem to be an option here. It
    will require to copy multiple types from
    `go-ethereum` on our side and handle conversions
    between them. Also, the aforementioned functions
    are operating on very low level (ABI) and their
    implementation may be forced to handle possible
    differences between chains.

--- pkg/chain/celo/celoutil/celoutil.go
+++ pkg/chain/celo/celoutil/celoutil.go
@@ -3,6 +3,7 @@ package celoutil
 import (
 	"context"
 	celo "github.com/celo-org/celo-blockchain"
+	"github.com/celo-org/celo-blockchain/accounts/abi"
 	"github.com/celo-org/celo-blockchain/accounts/abi/bind"
 	"github.com/celo-org/celo-blockchain/common"
 	"github.com/ipfs/go-log"
@@ -24,4 +25,97 @@ type CeloClient interface {
 		account common.Address,
 		blockNumber *big.Int,
 	) (*big.Int, error)
+}
+
+// CallAtBlock allows the invocation of a particular contract method at a
+// particular block. It papers over the fact that abigen bindings don't directly
+// support calling at a particular block, and is mostly meant for use from
+// generated contract code.
+func CallAtBlock(
+	fromAddress common.Address,
+	blockNumber *big.Int,
+	value *big.Int,
+	contractABI *abi.ABI,
+	caller bind.ContractCaller,
+	errorResolver *ErrorResolver,
+	contractAddress common.Address,
+	method string,
+	result interface{},
+	parameters ...interface{},
+) error {
+	input, err := contractABI.Pack(method, parameters...)
+	if err != nil {
+		return err
+	}
+
+	var (
+		msg = celo.CallMsg{
+			From:  fromAddress,
+			To:    &contractAddress,
+			Data:  input,
+			Value: value,
+		}
+		code   []byte
+		output []byte
+	)
+
+	output, err = caller.CallContract(context.TODO(), msg, blockNumber)
+	if err == nil && len(output) == 0 {
+		// Make sure we have a contract to operate on, and bail out otherwise.
+		if code, err = caller.CodeAt(
+			context.TODO(),
+			contractAddress,
+			nil,
+		); err != nil {
+			return err
+		} else if len(code) == 0 {
+			return bind.ErrNoCode
+		}
+	}
+
+	err = contractABI.Unpack(result, method, output)
+
+	if err != nil {
+		return errorResolver.ResolveError(
+			err,
+			fromAddress,
+			value,
+			method,
+			parameters...,
+		)
+	}
+
+	return nil
+}
+
+// EstimateGas tries to estimate the gas needed to execute a specific
+// transaction based on the current pending state of the backend blockchain.
+// There is no guarantee that this is the true gas limit requirement as other
+// transactions may be added or removed by miners, but it should provide a
+// basis for setting a reasonable default.
+func EstimateGas(
+	from common.Address,
+	to common.Address,
+	method string,
+	contractABI *abi.ABI,
+	transactor bind.ContractTransactor,
+	parameters ...interface{},
+) (uint64, error) {
+	input, err := contractABI.Pack(method, parameters...)
+	if err != nil {
+		return 0, err
+	}
+
+	msg := celo.CallMsg{
+		From: from,
+		To:   &to,
+		Data: input,
+	}
+
+	gas, err := transactor.EstimateGas(context.TODO(), msg)
+	if err != nil {
+		return 0, err
+	}
+
+	return gas, nil
 }

ac78143853d390dee4e38cb4727d6a66ecca01b9
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-08-25 15:45:58 +0200

    Define Chain ID for Ethereum Network
    Each ethereum network works with an associated Chain ID. Here we expose
    a function that returns a chain id for the network.

--- pkg/chain/ethereum/network.go
+++ pkg/chain/ethereum/network.go
@@ -14,3 +14,14 @@ const (
 func (n Network) String() string {
 	return []string{"unknown", "mainnet", "goerli", "developer"}[n]
 }
+
+// ChainID returns chain id associated with the network.
+func (n Network) ChainID() int64 {
+	switch n {
+	case Mainnet:
+		return 1
+	case Goerli:
+		return 5
+	}
+	return 0
+}

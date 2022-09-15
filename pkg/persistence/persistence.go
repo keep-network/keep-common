ad9d28a47055cb3b80d6ab8699fb45c3cb541907
Jakub Nowakowski <jakub.nowakowski@keep.network>
2022-09-15 14:04:33 +0200

    Separate persistance handles
    We want to have two types of persistence handles that store data on
    disk:
    - for sensitive data (e.g. keys) we don't want any data to be removed,
      but archived instead, we also need snapshots, this requires a specific
      directory structure with `current`, `archive` and `snapshot`
      subdirectories
    - for regular data (e.g. generated parameters) we want to store and
      delete data in a simple directory structure, without `current`
      subdirectory.
    
    Here we separate the interfaces for persistence handles:
    - RWHandle - common interface for reading and saving data
    - BasicHandle - simple interface for the regular data to save, read and
      remove them
    - ProtectedHandle - for sensitive data (i.e. keystore) to archive and
      snapshot instead of delete

--- pkg/persistence/persistence.go
+++ pkg/persistence/persistence.go
@@ -7,19 +7,13 @@ import "github.com/ipfs/go-log"
 
 var logger = log.Logger("keep-persistence")
 
-// Handle is an interface for data persistence. Underlying implementation
-// can write data e.g. to disk, cache, or hardware module.
-type Handle interface {
-
+// RWHandle is an interface for data persistence. Underlying implementation
+// can write and read data e.g. to disk, cache, or hardware module.
+type RWHandle interface {
 	// Save takes the provided data and persists it under the given name in the
 	// provided directory appropriate for the given persistent storage
 	// implementation.
 	Save(data []byte, directory string, name string) error
-
-	// Snapshot takes the provided data and persists it as an unique snapshot
-	// file in the provided directory appropriate for the given persistent
-	// storage implementation.
-	Snapshot(data []byte, directory string, name string) error
 
 	// ReadAll returns all non-archived data. It returns two channels: the first
 	// channel returned is a non-buffered channel streaming DataDescriptors of
@@ -28,14 +22,32 @@ type Handle interface {
 	// in a pipeline pattern. The function is non-blocking. Channels are closed
 	// when there is no more to be read.
 	ReadAll() (<-chan DataDescriptor, <-chan error)
+}
+
+// BasicHandle is an interface for data persistence. Underlying implementation
+// can read, write and remove data.
+type BasicHandle interface {
+	RWHandle
+
+	// Delete removes a file under the given name in the provided directory
+	// appropriate for the given persistent storage implementation.
+	Delete(directory string, name string) error
+}
+
+// ProtectedHandle is an interface for data persistence. Underlying implementation
+// can read and write data, but it cannot remove the data. Instead of removing
+// the data it can archive them or take snapshots.
+type ProtectedHandle interface {
+	RWHandle
 
 	// Archive marks the entire directory with the name provided as archived
 	// so that the data in that directory is not returned from ReadAll.
 	Archive(directory string) error
 
-	// Delete removes a file under the given name in the provided directory
-	// appropriate for the given persistent storage implementation.
-	Delete(directory string, name string) error
+	// Snapshot takes the provided data and persists it as an unique snapshot
+	// file in the provided directory appropriate for the given persistent
+	// storage implementation.
+	Snapshot(data []byte, directory string, name string) error
 }
 
 // DataDescriptor is an interface representing data saved in the persistence
